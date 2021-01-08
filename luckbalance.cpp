0c2439bc51799d4fc33c591361cd50d77b94b04f
Sharadha Srinivasan <ssharadhas@gmail.com>
2021-01-08 22:18:01 +0400

    Create luckbalance.cpp

---
+++ luckbalance.cpp
@@ -0,0 +1,93 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> split_string(string);
+
+// Complete the luckBalance function below.
+int luckBalance(int k, vector<vector<int>> contests,int n) {
+int a[10000],m=0,sum=0,i;
+for(i=0;i<n;i++)
+{
+    if(contests[i][1]==1)
+    {a[m]=contests[i][0];
+    m++;
+    }
+}
+sort(a,a+m,greater<int>());
+for(i=0;i<m;i++)
+{
+    if((i+1)<=k)
+    sum=sum+a[i];
+    else
+    sum=sum-a[i];
+}
+for(i=0;i<n;i++)
+{
+    if(contests[i][1]==0)
+    sum=sum+contests[i][0];
+}
+return sum;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string nk_temp;
+    getline(cin, nk_temp);
+
+    vector<string> nk = split_string(nk_temp);
+
+    int n = stoi(nk[0]);
+
+    int k = stoi(nk[1]);
+
+    vector<vector<int>> contests(n);
+    for (int i = 0; i < n; i++) {
+        contests[i].resize(2);
+
+        for (int j = 0; j < 2; j++) {
+            cin >> contests[i][j];
+        }
+
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    }
+
+    int result = luckBalance(k, contests,n);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
